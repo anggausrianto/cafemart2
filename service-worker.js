2cc240f849b992b096db0026bc2cec1e1ad1e4ea
anggausrianto <anggausrianto@gmail.com>
2026-01-08 10:34:15 +0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install',e=>{
- e.waitUntil(
-  caches.open('cafemart-pro-cache').then(cache=>{
-   return cache.addAll(['./','./index.html','./app.html','./icon-192.png','./icon-512.png']);
-  })
- );
-});
